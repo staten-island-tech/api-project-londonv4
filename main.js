336b4e1921953e8f81fb4f2297eea33030c9927a
londonv4 <londonv4@nycstudents.net>
2026-01-05 12:31:17 -0500

    l

---
+++ main.js
@@ -0,0 +1,20 @@
+const url= "https://holidays.abstractapi.com/v1/?api_key=c3bd703352004180b80425fb650ad860&country=US"
+async function getData() {
+    try {
+        //go get data
+const response = await fetch();
+// handle errors
+if(response.status !=200){
+    throw new error(response);
+} else {
+    // makes the response into json data we can use
+    const data = await response.json();
+    console.log(data);
+    document.getElementById("").textContent=data.name;
+}
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+getData("");
