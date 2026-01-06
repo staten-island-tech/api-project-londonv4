ace6f47c7b9356562cf8fe6a47e20dca1a6334f1
londonv4 <londonv4@nycstudents.net>
2026-01-06 12:31:30 -0500

    tue

--- API project/main.js
+++ API project/main.js
@@ -2,7 +2,7 @@ const url= "https://holidays.abstractapi.com/v1/?api_key=c3bd703352004180b80425f
 async function getData() {
     try {
         //go get data
-const response = await fetch();
+const response = await fetch(url);
 // handle errors
 if(response.status !=200){
     throw new error(response);
@@ -10,10 +10,10 @@ if(response.status !=200){
     // makes the response into json data we can use
     const data = await response.json();
     console.log(data);
-    document.getElementById("").textContent=data.name;
+    // document.getElementById("").textContent=data.name;
 }
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }
     
 }
