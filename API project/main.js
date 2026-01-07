f0ef8d67b60deae2b0a132356b06e75df034ce02
londonv4 <londonv4@nycstudents.net>
2026-01-07 12:30:56 -0500

    ok

--- API project/main.js
+++ API project/main.js
@@ -1,20 +1,39 @@
-const url= "https://holidays.abstractapi.com/v1/?api_key=c3bd703352004180b80425fb650ad860&country=US"
-async function getData() {
-    try {
-        //go get data
-const response = await fetch(url);
-// handle errors
-if(response.status !=200){
-    throw new error(response);
-} else {
-    // makes the response into json data we can use
-    const data = await response.json();
-    console.log(data);
-    // document.getElementById("").textContent=data.name;
-}
-    } catch (error) {
-        console.error(error);
-    }
+// const url= "c3bd703352004180b80425fb650ad860&country=US"
+// async function getData(url) {
+//     try {
+//         //go get data
+// const response = await fetch(url);
+// // handle errors
+// if(response.status !=200){
+//     throw new error(response);
+// } else {
+//     // makes the response into json data we can use
+//     const data = await response.json();
+//     console.log(data);
+//     // document.getElementById("").textContent=data.name;
+// }
+//     } catch (error) {
+//         console.error(error);
+//     }
     
+// }
+// getData("");
+
+const url = "c3bd703352004180b80425fb650ad860&country=US";
+
+console.log(fetch(url));
+
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
 }
-getData("");
+fetchData(url);
